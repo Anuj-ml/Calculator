2a2bbf17effb2c849ff0d75e96be75dc25a20cf0
Anuj Jha <anujjha1004@gmail.com>
2025-08-05 22:36:42 +0530

    Toggle button for switching modes added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
-let input = document.getElementById('inputBox');
+let input = document.querySelector('.inputBox');
 let btns = document.querySelectorAll('.button');
-
+let mode = document.querySelector('#mode');
+let calc = document.querySelector('.calculator');
+let icons = document.querySelector('.icons');
+let operators = document.querySelectorAll('.button.operator');
 
 let result = '';
 
@@ -9,6 +12,9 @@ btns.forEach((btn) => {
     btn.addEventListener("click", () => {
         console.log(btn.innerText)
         if (btn.innerText == "=") {
+            if((input.value).search('%')) {
+                input.value = (input.value).replace('%', '/100');
+            }
             result = eval(input.value);
             input.value = result;
         }
@@ -34,3 +40,30 @@ btns.forEach((btn) => {
 }
 )
 
+document.addEventListener('keydown', (e) => {
+    if(e.key == 'Enter'){
+        if((input.value).search('%')) {
+                input.value = (input.value).replace('%', '/100');
+        }
+        console.log(input.value);
+        result = eval(input.value);
+        console.log(result);
+        input.value = result;
+        console.log(input.value);
+        e.preventDefault();
+    }
+})
+
+
+mode.addEventListener("click", () => {
+    calc.classList.toggle('light');
+    icons.classList.toggle('light');
+    input.classList.toggle('light');
+    btns.forEach((btn) => {
+        btn.classList.toggle('light-shadow');
+    })
+    operators.forEach((operator) => {
+        operator.classList.toggle('light');
+    })
+
+})
